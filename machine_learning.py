02919c34abc1dc94d9f06498bb3453e980e157dc
cjromaa <117147142+cjromaa@users.noreply.github.com>
2024-01-01 13:52:12 -0700

    Add files via upload

---
+++ machine_learning.py
@@ -0,0 +1,178 @@
+import cv2
+import os
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from keras.models import Sequential, load_model
+from keras.layers import Dense
+from keras.optimizers import SGD
+import numpy as np
+import pickle
+#import main
+
+# Cascades
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+
+# List of folder paths
+folder_paths = [
+    " "
+    # Enter required folder paths of faces you want to train
+]
+
+# Machine Learning Section
+def machine_learning():
+    # These image paths are used for testing and seeing how accurate the machine learning is
+    image_path = ''
+    image_path2 = ''
+    image_path3 = ''
+    image_path4 = ''
+    image_path5 = ''
+    image_path6 = ''
+    image_path7 = ''
+
+    # Test images
+    data = []
+    labels = []
+    for folder_path in folder_paths:
+        for filename in os.listdir(folder_path):
+            imagepath = os.path.join(folder_path, filename)
+            image = cv2.imread(imagepath)
+            image = cv2.resize(image, (32, 32))
+            image = image.flatten()
+            data.append(image)
+
+            # Extracting the class label from the image path and update the labels list
+            label = imagepath.split(os.path.sep) [-2]
+            labels.append(label)
+    print(data) 
+    print(labels)
+
+    # Scale the raw pixel intensities to the range [0, 1]
+    data = np.array(data, dtype="float") / 255.0
+    labels = np.array(labels)
+    print(data)
+    print(labels)  
+
+    x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=42)
+    # convert the labels from integers to vectors 
+    lb = LabelBinarizer()
+    y_train = lb.fit_transform(y_train)
+    y_test = lb.transform(y_test)
+
+    # define the 3072-1024-512-3 architecture using Keras
+    model = Sequential()
+    model.add(Dense(1024, input_shape=(3072,), activation="sigmoid"))
+    model.add(Dense(512, activation="sigmoid"))
+    model.add(Dense(len(lb.classes_), activation="softmax"))
+
+    # Learning Rate
+    INIT_LR = 0.01
+    EPOCHS = 150
+
+    # Compiling the model
+    print("[INFO] training network...")
+    opt = SGD(lr=INIT_LR)
+    model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
+    H = model.fit(x=x_train, y=y_train, validation_data=(x_test, y_test),
+	epochs=EPOCHS, batch_size=32)
+
+    # Save the model and label binarizer
+    model.save('my_model.h5')
+    with open('lb.pickle', 'wb') as file:
+        pickle.dump(lb, file)
+
+    # Load the model for prediction
+    model = load_model('my_model.h5')
+
+
+    # Process the image for prediction
+    img = cv2.imread(image_path)
+    img = cv2.resize(img, (32, 32))
+    img = img.flatten()
+    img = np.array([img], dtype="float") / 255.0
+
+    # Unedited img for CV
+    unedited_img = cv2.imread(image_path5, 0)
+    
+    # Make a prediction
+    predictions = model.predict(img)
+    print(predictions)
+    i = predictions.argmax(axis=1) [0]
+    print(i)
+    real_label = lb.classes_[i]
+    print("Classes:", lb.classes_)
+
+    text = "{}: {:.2f}%".format(real_label, predictions[0][i] * 100)
+
+    # Determining if the face meets the proper criteria of 80% match
+    failed = "Does not meet criteria, press [S] to try again"
+        
+    if predictions[0][i] * 100 < 70:
+        print(text)
+        print(failed)
+    else:
+        print(text)
+    return model, lb, x_train, x_test, y_train, y_test, data
+
+# section is used for my main.py code to access
+def analyze(filename, model, lb):
+    # Load the model for prediction
+    c = 0 
+    # Runs the picture 3x to make confirm that it's the right person
+    while c < 3:
+        model = load_model('my_model.h5')
+
+        # Process the image for prediction
+        img = cv2.imread(filename)
+        img = cv2.resize(img, (32, 32))
+        img = img.flatten()
+        img = np.array([img], dtype="float") / 255.0
+            
+        # Make a prediction
+        predictions = model.predict(img)
+        #print(predictions)
+        i = predictions.argmax(axis=1) [0]
+        #print(i)
+        real_label = lb.classes_[i]
+
+        text = "{}: {:.2f}%".format(real_label, predictions[0][i] * 100)
+        #print(text)
+
+        # Determining if the face meets the proper criteria of 80% match
+        failed = "Does not meet criteria, press [S] to try again"
+
+        if predictions[0][i] * 100 < 70:
+            return text, failed, lb.classes_
+        
+        # i represents what face matches with what person
+        # Right now we only tested with 6 people but you are welcomed to chang anything if needed
+        # You can print the "lb.classes" to see what number matches with the individual
+        if i == 0:
+            print("Face detected in image: ")
+            face = ''
+        elif i == 1:
+            print("Face detected in image: ")
+            face = ''
+        elif i == 2:
+            print("Face detected in image: ")
+            face = ''
+        elif i == 3:
+            print("Face detected in image: ")
+            face = ''
+        elif i == 4:
+            print("Face detected in image: ")
+            face = ''
+        elif i == 5:
+            print("Face detected in image: ")
+            face = ''
+        elif i == 6:
+            print("Face detected in image: ")
+            face = ''
+            
+        else:
+            print("Error")
+        print("Classes:", lb.classes_)
+        c+=1
+    return text, face, lb.classes_
+
+machine_learning()
---
+++ main.py
@@ -0,0 +1,230 @@
+import cv2
+import os
+import openai
+from openai import OpenAI
+import mysql.connector
+from mysql.connector import Error
+import tensorflow as tf
+from keras.models import Sequential, load_model
+import pickle
+from datetime import datetime
+from machine_learning import machine_learning
+from machine_learning import analyze
+
+# Cascades
+face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+
+# Folder path
+folder_path = "" # Enter your folder path
+
+# Using GPT API to create HTML page that has individual information
+def gpt_api(gpt_info, name, filename):
+    print("Building Personal HTML page....")
+    main_prompt = f"Create an HTML page that displays information about {name}. Include the following details: {gpt_info}. Also include the photo {filename} into the website. Format it neatly with headers and paragraphs and use CSS to style it and make it look professional and appealing"
+    try:
+        client = OpenAI(api_key=" ") # Enter your GPT API key
+        # Generate HTML Content
+        response = client.chat.completions.create(
+            model = "gpt-4-1106-preview",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You will be provided instructions on how to build an HTML page of invidiuals information"
+                },
+                {
+                    "role": "user",
+                    "content": main_prompt
+                }
+            ],
+            #temperature=0.7,
+            stop="</html>"
+        )
+        
+        # Extract and print HTML content
+        html_content = response.choices[0].message.content
+
+        # Finding certain word so it only writes the relevant information
+        word_to_find = "<!DOCTYPE html>"
+        index = html_content.find(word_to_find)
+        if index != -1:
+            content_to_write = html_content[index:]
+            with open("test.html", "w") as html_file:
+                html_file.write(content_to_write)
+        else:
+            print("Word not found")
+        #print(content_to_write)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    
+    # Starting up the file automatically
+    try:
+        os.startfile("D:\\cidjo\\Documents\\Python Coding\\Capstone Project\\Face_Secure\\test.html")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    
+    
+# Connecting to SQL database
+def sql_connect():
+    try:
+        # Connect to the MySQL database
+        db = mysql.connector.connect(
+            host="",                    # Enter your database
+            user="",                    # Enter your MySQL username
+            passwd="",                  # Enter your password to your database
+            database=""                 # Enter the database you want to access
+        )
+        if db.is_connected():
+            print('Successfully connected to the database.')
+    except Error as e:
+        print(f"The error '{e}' occurred")
+        exit()
+
+    db.close()
+
+# Connecting to database and quering information for given target
+def access_database(name, filename):
+    try:
+        # Connect to the MySQL database
+        db = mysql.connector.connect(
+            host="",                    # Enter your database
+            user="",                    # Enter your MySQL username
+            passwd="",                  # Enter your password to your database
+            database=""                 # Enter the database you want to access
+        )
+        if db.is_connected():
+            print(f'Querying {name}.......')
+        cursor = db.cursor()
+
+        # Function to fetch and print information
+        def fetch_and_print_info(query, information):
+            chat_gpt_info = []
+            cursor.execute(query)
+            results = cursor.fetchall()
+            for row in results:
+                if name in row:
+                    information = f"{information}{row[3]}"
+                    chat_gpt_info.append(information)
+            return chat_gpt_info
+            
+        # Information that's gonna be sent to GPT 4 API
+        gpt_info = []
+
+        # Getting Gender Information
+        gender = "Gender Information: "
+        gpt_info.extend(fetch_and_print_info("SELECT * FROM users JOIN gender_info ON users.id = gender_info.users_id", gender))
+
+        # Getting Ethnicity Information
+        ethnicity = "Ethnicity Information: "
+        gpt_info.extend(fetch_and_print_info("SELECT * FROM users JOIN ethnicity_info ON users.id = ethnicity_info.users_id", ethnicity))
+
+        # Getting Address Information
+        address = "Address Information: "
+        gpt_info.extend(fetch_and_print_info("SELECT * FROM users JOIN address_info ON users.id = address_info.users_id", address))
+    
+        # Getting Occupation Information
+        occupation = "Occupation Information: "
+        gpt_info.extend(fetch_and_print_info("SELECT * FROM users JOIN occupation_info ON users.id = occupation_info.users_id", occupation))
+
+        # Getting School Information
+        school = "School Information: "
+        gpt_info.extend(fetch_and_print_info("SELECT * FROM users JOIN school_info ON users.id = school_info.users_id", school))
+
+        #print(gpt_info)
+        #print(name)
+        gpt_api(gpt_info, name, filename)
+
+    except Error as e:
+        print(f"The error '{e}' occurred")
+    finally:
+        if db.is_connected():
+            cursor.close()
+            db.close()
+            print("MySQL connection is closed")
+
+def face_capture():
+    cap = cv2.VideoCapture(0)
+    last_capture_time = None
+    capture_delay = 0  # seconds delay between captures
+    text = 'Analyzing....'
+    
+    # Load model and LabelBinarizer here
+    model = load_model('my_model.h5')
+    with open('lb.pickle', 'rb') as file:
+        lb = pickle.load(file)
+
+    try:
+        while True:
+            ret, frame = cap.read()
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+            x, y, w, h = 0, 0, 0, 0  
+
+            for (x, y, w, h) in faces:
+                # Extract the region of interest (the face)
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+                roi_gray = frame[y:y+h, x:x+w]
+
+                # Resize the face region to 150x150 pixels
+                roi_gray_resized = cv2.resize(roi_gray, (150, 150))
+
+                # Check if enough time has passed since the last capture
+                if cv2.waitKey(1) & 0xFF == ord('s'):
+                    current_time = datetime.now()
+                    if last_capture_time is None or (current_time - last_capture_time).seconds > capture_delay:
+                        # Generate a unique filename with timestamp
+                        timestamp = current_time.strftime("%Y%m%d_%H%M%S")
+                        filename = f"face_image_{timestamp}.jpg"
+                        current_path = "D:\\cidjo\\Documents\\Python Coding\\Capstone Project\\Face_Secure"
+                        image_path = os.path.join(current_path, filename)
+                        
+
+                        # Save the resized image in saved_faces folder
+                        cv2.imwrite(image_path, roi_gray_resized)
+                        print(f"Image saved successfully as {filename}")
+                        last_capture_time = current_time
+
+                        # Sending the photo to the machine learning to determine who the person is
+                        person_identified, individual, classes = analyze(image_path, model, lb)
+                        print("Person Identified:", person_identified)
+                        person_identified_text = f'{person_identified}...'
+
+                        # Font for person_identified
+                        font = cv2.FONT_HERSHEY_SIMPLEX
+                        fontScale = 1
+                        color = (0, 255, 0)  # Green color in BGR
+                        thickness = 2
+
+                        # Put the text on the frame
+                        cv2.putText(frame, person_identified_text, (x, y - 10), font, fontScale, color, thickness)
+                        text = person_identified_text
+
+                        # Accessing and Querying SQL database
+                        if individual in classes:
+                            #print(classes)
+                            access_database(individual, filename)
+                        else:
+                            print(individual) # This will print that the user is not recognized in the database 
+            
+            # Font for person_identified
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            fontScale = 1
+            color = (0, 255, 0)  # Green color in BGR
+            thickness = 2
+
+            cv2.putText(frame, text, (x, y - 10), font, fontScale, color, thickness)
+                        
+            cv2.imshow('Face Secure Authentication', frame)
+            
+            # Breaking the capture
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                print("Exiting system")
+                break
+    except KeyboardInterrupt:
+        print("Interrupted by user, shutting down.")
+    finally:
+        # Release the capture and close any open windows
+        cap.release()
+        cv2.destroyAllWindows()
+
+if sql_connect() == None:
+    face_capture()
